d9fddc9f860b7554bf25748d4cdc95429a79fc7d
takemichi <hoangbru07@gmail.com>
2023-07-21 21:59:05 +0700

    feat: create routing
    Create router for page:
    - Home
    - Liked movie
    - Movie, movie detail
    - Signin

---
+++ src/app/movie/[movieId]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const metadata = {
+    title: `Wimovie | Movie - `
+}
+
+const page = () => {
+  return (
+    <div>Movie detail</div>
+  )
+}
+
+export default page
